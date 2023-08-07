a7e62a18ac43f73cacfb1e2f964869d7e8114fff
SKolodkin <dev1@instrument-fit.ru>
2023-08-07 09:05:49 +0300

    start create todo

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+from todos.todo import todo_router
+
+
 app = FastAPI()
 
 
 @app.get("/")
 async def welcome() -> dict:
-    return {"message": "Hello, World!"}
+    return {"message": "Hello!"}
+
+app.include_router(todo_router)
